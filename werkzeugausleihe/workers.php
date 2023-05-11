f505825a66b47023b960a39e402e7a52c14f859a
UnknownUser95 <luluklzde@gmail.com>
2023-05-11 18:17:02 +0200

    initial state

---
+++ werkzeugausleihe/workers.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Workers</title>
+<link rel="stylesheet" type="text/css" href="workers.css">
+</head>
+<body>
+	<div class="table">
+		<div class="table-header-row table-row">
+			<div class="table-content table-header">ID</div>
+			<div class="table-content table-header">Vorname</div>
+			<div class="table-content table-header">Nachname</div>
+			<div class="table-content table-header">Geburtsdatum</div>
+		</div>
+		<?php
+		require_once 'db.php';
+
+		$result = $conn->query("SELECT * FROM mitarbeiter");
+
+		function output($row, $name) {
+			echo "<div class='table-content'>".$row[$name]."</div>";
+		}
+
+		if($result->num_rows > 0) {
+			// output data of each row
+			while($row = $result->fetch_assoc()) {
+				echo "<div class='table-row'>";
+				output($row, "mitarbeiternr");
+				output($row, "vorname");
+				output($row, "nachname");
+				output($row, "geburtsdatum");
+				echo "</div>";
+			}
+		}
+		?>
+	</div>
+</body>
+</html>
